d3f13018c7bf26f225ec2d43f2aa83b2e70b6d14
ersaazis <eam24maret@gmail.com>
2020-02-03 21:05:51 +0700

    update builder

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ ea-php72 -r "unlink('composer.phar');"
 mv app/Http/Controllers app/Http/Controllersx
 ea-php72 artisan dev:install
 mv app/Http/Controllersx app/Http/Controllers
+cp -r resources/lang/id vendor/crocodicstudio/crudbooster/src/localization 
