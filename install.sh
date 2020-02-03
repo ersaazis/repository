d89b9e63efd0aff3d8e4ca42772ff59fd97b3779
ersaazis <eam24maret@gmail.com>
2020-02-03 21:34:53 +0700

    update builder

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ea-php72 -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ea-php72 -r "if (hash_file('sha384', 'composer-setup.php') === 'c5b9b6d368201a9db6f74e2611495f369991b72d9c8cbd3ffbc63edff210eb73d46ffbfce88669ad33695ef77dc76976') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+#ea-php72 -r "if (hash_file('sha384', 'composer-setup.php') === 'c5b9b6d368201a9db6f74e2611495f369991b72d9c8cbd3ffbc63edff210eb73d46ffbfce88669ad33695ef77dc76976') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 ea-php72 composer-setup.php
 ea-php72 -r "unlink('composer-setup.php');"
 ea-php72 composer.phar install
