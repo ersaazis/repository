f1f247e4cbd4f2532d9cd63e348788fcc20e3bd8
ersaazis <eam24maret@gmail.com>
2020-02-04 18:01:04 +0700

    update viewer

--- resources/views/previewRepo.blade.php
+++ resources/views/previewRepo.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="row">
     <div class="col-12">
-        <iframe style="height:100vh; width:100%; border:none; margin:0; padding:0; overflow:hidden;" src="https://docs.google.com/viewer?url={{url()->current()}}&hl=en&pid=explorer&efh=false&a=v&chrome=false&embedded=true"></iframe>
+        <iframe style="height:100vh; width:100%; border:none; margin:0; padding:0; overflow:hidden;" src="https://docs.google.com/viewer?url={{urlencode(url()->current('?download=yes'))}}?download=yes&hl=en&pid=explorer&efh=false&a=v&chrome=false&embedded=true"></iframe>
     </div>
     <div class="col-12">
         <a class="btn btn-secondary float-left" role="button" href="{{url()->current()}}/../"><i class="fa fa-backward"></i>Back</a>
